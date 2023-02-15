60ab62c7082846871e9d6913ea4aed343b8c306f
Justyna Boesche <justyna.boesche@gmail.com>
2023-02-15 09:07:02 +0100

    GlobalStyle.js file created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import background from "../src/background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+    
+  *, ::after, ::before {
+    box-sizing:inherit;
+  }
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background-color: ${({ theme }) => theme.color.gallery};
+  }
+
+  #root {
+  font-family: 'Montserrat', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-image: url("${background}");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-color: white;
+}
+`;
