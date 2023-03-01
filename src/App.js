b343d31f4363527ef4b72df44aa0267d476ec949
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-01 09:45:52 +0100

    Footer component from app removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Form } from "./Form";
-import {Footer} from "./Footer";
-import {Container} from "./Container/styled";
+import { Container } from "./Container/styled";
 
 function App() {
   return (
     <Container>
       <Form />
-      <Footer description="Kursy walut według średniego kursu NBP z dn. 28.10.2022" />
     </Container>
   );
 }
