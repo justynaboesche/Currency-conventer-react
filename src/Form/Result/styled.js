7b39bbc4c2d0f5f5367296a1e0446d1e7686ae26
Justyna Boesche <justyna.boesche@gmail.com>
2023-03-01 10:42:40 +0100

    padding in StyledResult changed

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledResult = styled.p`
     color: ${({ theme }) => theme.color.stromboli};
     font-size: 20px;
-    padding-top: 10px;
+    padding: 10px;
 `;
 
 
